f52a5b1400abc4e300d61547b5de5cabc4e56a60
Enes Somuncular <somuncular13@gmail.com>
2026-03-25 11:47:21 +0300

    feat(menu): persistent player name saving using SettingsManager

---
+++ src/com/zomtopia/utils/SettingsManager.java
@@ -0,0 +1,41 @@
+package com.zomtopia.utils;
+
+import java.io.*;
+import java.util.Properties;
+
+public class SettingsManager {
+    private static final String SETTINGS_FILE = "settings.properties";
+    private static Properties properties = new Properties();
+
+    static {
+        load();
+    }
+
+    public static void load() {
+        File file = new File(SETTINGS_FILE);
+        if (file.exists()) {
+            try (InputStream input = new FileInputStream(file)) {
+                properties.load(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void save() {
+        try (OutputStream output = new FileOutputStream(SETTINGS_FILE)) {
+            properties.store(output, "Zomtopia Settings");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getPlayerName() {
+        return properties.getProperty("playerName", "");
+    }
+
+    public static void setPlayerName(String name) {
+        properties.setProperty("playerName", name);
+        save();
+    }
+}
